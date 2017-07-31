1ea3ac86a8f1f64f5c82820bb136b6a79a03f215
yashg7cr <yashwin.yashwin3@gmail.com>
2017-07-31 12:10:22 +0530

    partner
    partner

---
+++ cr7/Commerce-API-DotNet-master/Program.cs
@@ -0,0 +1,137 @@
+﻿/********************************************************
+*                                                        *
+*   Copyright (C) Microsoft. All rights reserved.        *
+*                                                        *
+*********************************************************/
+
+
+namespace Microsoft.Partner.CSP.Api.V1.Samples
+{
+	using System;
+	using System.Configuration;
+
+	static class Program
+	{
+
+		static void Main()
+		{
+			// This is the Microsoft ID of the reseller
+			// Please work with your Admin Agent to get it from https://partnercenter.microsoft.com/en-us/pc/AccountSettings/TenantProfile
+			string microsoftId = ConfigurationManager.AppSettings["MicrosoftId"];
+
+			// This is the default domain of the reseller
+			// Please work with your Admin Agent to get it from https://partnercenter.microsoft.com/en-us/pc/AccountSettings/TenantProfile
+			string defaultDomain = ConfigurationManager.AppSettings["DefaultDomain"];
+
+			// This is the appid that is registered for this application in Azure Active Directory (AAD)
+			// Please work with your Admin Agent to get it from  https://partnercenter.microsoft.com/en-us/pc/ApiIntegration/Overview 
+			string appId = ConfigurationManager.AppSettings["AppId"];
+
+			// This is the key for this application in Azure Active Directory
+			// This is only available at the time your admin agent has created a new app at https://partnercenter.microsoft.com/en-us/pc/ApiIntegration/Overview
+			// You could alternatively goto Azure Active Directory and generate a new key, and use that.
+			string key = ConfigurationManager.AppSettings["Key"];
+
+			string ExistingCustomerMicrosoftId = ConfigurationManager.AppSettings["ExistingCustomerMicrosoftId"];
+
+
+			// This is the clientId for this application in Azure Active Directory
+			// This is only available at the time you created a new app at
+			// "https://manage.windowsazure.com/#Workspaces/ActiveDirectoryExtension/directory"
+
+			//  Configure application 
+			//		Multi-tenant
+			//    Permission to Windows Azure Service Management API
+			//		Pre-consent
+			//  
+			string azureAppId = ConfigurationManager.AppSettings["AzureAppId"];
+			string credentialName = ConfigurationManager.AppSettings["CredentialName"];
+
+
+
+			// Prompts the user to edit the config parametres if its not already done.
+			Utilities.ValidateConfiguration(microsoftId, defaultDomain, appId, key, ExistingCustomerMicrosoftId,
+				azureAppId, credentialName);
+
+			// Gets the scenario from app config. If an invalid entry is found, prompts the user to enter a valid value.
+			string scenario = ConfigurationManager.AppSettings["scenario"];
+			int intScenario;
+			bool isParsed = int.TryParse(scenario, out intScenario);
+			if (!isParsed || intScenario < 0 || intScenario > 8)
+			{
+				Utilities.PromptForScenario();
+			}
+
+			try
+			{
+				if (intScenario == Constants.SCENARIO_ONE || intScenario == Constants.ALL_SCENARIOS)
+				{
+					var customer = Orchestrator.GetCustomer(defaultDomain, appId, key, ExistingCustomerMicrosoftId, microsoftId);
+				}
+				if (intScenario == Constants.SCENARIO_TWO || intScenario == Constants.ALL_SCENARIOS)
+				{
+					Orchestrator.GetSubscriptions(defaultDomain, appId, key, ExistingCustomerMicrosoftId, microsoftId);
+				}
+				if (intScenario == Constants.SCENARIO_THREE || intScenario == Constants.ALL_SCENARIOS)
+				{
+					Orchestrator.GetOrders(defaultDomain, appId, key, ExistingCustomerMicrosoftId, microsoftId);
+				}
+				if (intScenario == Constants.SCENARIO_FOUR || intScenario == Constants.ALL_SCENARIOS)
+				{
+					Orchestrator.CreateOrder(defaultDomain, appId, key, ExistingCustomerMicrosoftId, microsoftId);
+				}
+				if (intScenario == Constants.SCENARIO_FIVE || intScenario == Constants.ALL_SCENARIOS)
+				{
+					Orchestrator.GetRateCardAndUsage(defaultDomain, appId, key, ExistingCustomerMicrosoftId, microsoftId);
+				}
+				if (intScenario == Constants.SCENARIO_SIX || intScenario == Constants.ALL_SCENARIOS)
+				{
+					var subscriptionId = ConfigurationManager.AppSettings["Office365SubscriptionId"];
+					if (string.IsNullOrWhiteSpace(subscriptionId) || subscriptionId.Equals("Office365 Subscription Id"))
+						Utilities.PromptForSubscriptionId();
+					Orchestrator.TransitionToNewSKU(subscriptionId, defaultDomain, appId, key, ExistingCustomerMicrosoftId, microsoftId);
+				}
+				if (intScenario == Constants.SCENARIO_SEVEN || intScenario == Constants.ALL_SCENARIOS)
+				{
+					Orchestrator.ListAndDeleteAllCustomers(defaultDomain, appId, key, microsoftId, ExistingCustomerMicrosoftId);
+				}
+				if (intScenario == Constants.SCENARIO_SEVEN || intScenario == Constants.ALL_SCENARIOS)
+				{
+					Orchestrator.CreateCustomer(defaultDomain, appId, key, microsoftId);
+				}
+				if (intScenario == Constants.SCENARIO_EIGHT || intScenario == Constants.ALL_SCENARIOS)
+				{
+					Orchestrator.CreateVirtualMachineInNewSubscription(defaultDomain, appId, key, microsoftId, azureAppId, credentialName);
+				}
+			}
+			catch (System.FieldAccessException)
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine("\n\n\n Looks like you are debugging the application.  Inorder to fix this exception: "
+						+ "\n 1. In Visual Studio, Right Click on the Project Microsoft.CSP.Api.V1.Samples"
+						+ "\n 2. Select the Debug Tab"
+						+ "\n 3. Uncheck the option \"Enable the Visual Studio hosting process\" (it is at the bottom of the page)"
+						+ "\n 4. Save the changes (File -> Save Selected Items)"
+						+ "\n 5. Debug the app now.");
+				Console.Write("Make sure you copy/remember the above steps before exiting the app.");
+			}
+			catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException)
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine("\n Make sure the app.config has all the right settings.  The defaults in the app.config won't work."
+						+ "\n If the settings are correct, its possible you are hitting a service error.  Try again."
+						+ "\n If the error persists, contact support");
+			}
+			catch (Exception ex)
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine("\n An unrecoverable error occurred:");
+				Console.WriteLine(ex.Message);
+			}
+
+			Console.ResetColor();
+			Console.Write("\n\n\nHit enter to exit the app...");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ cr7/Commerce-API-DotNet-master/README.md
@@ -0,0 +1,37 @@
+# Commerce-API-DotNet
+
+These are samples in C# for using the commerce APIs for Microsoft Partner Center. 
+These CREST APIs are documented at [https://msdn.microsoft.com/en-us/library/partnercenter/dn974944.aspx](https://msdn.microsoft.com/en-us/library/partnercenter/dn974944.aspx).
+
+A public forum for discussing the APIs is available at 
+[https://social.msdn.microsoft.com/Forums/en-US/home?forum=partnercenterapi](https://social.msdn.microsoft.com/Forums/en-US/home?forum=partnercenterapi).
+
+## Prerequisites
+
+To update resources in a customer tenant or directory, an application must have a security principal. The
+principal can be created in every customer tenant, or can be created in the reseller tenant via a multi-tenant,
+native application. To ease the admistrative burden for the sample, the latter option is implemented. The
+steps are detailed in the [prerequisite page](Documentation/Prerequisite.md).
+
+The Create VM scenario will require user credentials at run-time. This user must be a member of the
+Admin Agents group in the reseller directory. To allow unattended operation, the sample application 
+uses the Windows Credential Manager application to securely store the account and password of this user.
+It is recommended that a "service account" be created rather than storing an actual user credentials.
+The [Credential Manager](Documentation/CredentialManager.md) page includes steps for creating a 
+stored credential.
+
+## App Configuration
+
+The sample application has several configurable settings in the app.config file. Set the values as
+appropriate for your tenant.
+
+Configuration Key | Description
+------------ | -------------
+scenario | The scenario to execute. A value of zero will run all scenarios.
+AppId | The id of the application registered in the Partner Center. This id is used to access the CREST APIs.
+Key | The key of the application registered in the Partner Center. 
+MicrosoftId | The Microsoft Id of the reseller. Used to access CREST APIs.
+DefaultDomain | The default domain of the reseller in Microsoft Azure. (This is typically an "onmicrosoft.com" domain.)
+ExistingCustomerMicrosoftId | The Microsoft Id of a customer associated with the reseller. This is used in scenarios that require a customer id, such as GetSubscriptions and GetOrders.
+AzureAppId | The id of the multi-tenant application registered in the reseller directory that provisions resources in the customer tenant.
+CredentialName | The value of "Internet or network address" in Credential Manager that contains the service account credentials.
